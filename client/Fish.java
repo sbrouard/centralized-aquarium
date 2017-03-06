e6f6ad940df225cac11de83601dcfe8ce3999911
BROUARD Simon <sbrouard@quinte.pedago.ipb.fr>
2017-03-06 12:25:47 +0100

    debut parseur config

---
+++ client/Fish.java
@@ -0,0 +1,50 @@
+public class Fish{
+
+    int pos_x;
+    int pos_y;
+    int width;
+    int height;
+    String name;
+    String type;
+    
+    public Fish(String name, String type,int pos_x,int pos_y,int width,int height){
+	this.name = name;
+	this.type = type;
+	this.pos_x = pos_x;
+	this.pos_y = pos_y;
+	this.width = width;
+	this.height = height;
+    }
+
+    public String getName(){
+	return name;
+    }
+
+    public String getType(){
+	return type;
+    }
+
+    public int getPosX(){
+	return pos_x;
+    }
+    
+    public int getPosY(){
+	return pos_y;
+    }
+
+    public int getWidth(){
+	return width;
+    }
+
+    public int getHeight(){
+	return height;
+    }
+
+    public int setPosX(int newPosX){
+	pos_x = newPosX;
+    }
+
+ public int setPosY(int newPosY){
+	pos_y = newPosY;
+    }
+}
---
+++ client/tst/TestConfigParser.java
@@ -0,0 +1,28 @@
+public class TestConfigParser {
+    
+    public void testParser(){
+	ConfigParser cfgp = new ConfigParser("affichage.cfg");
+	assert("127.0.0.1".equals(cfgp.getControllerAddress()));
+	assert(cfgp.getControllerPort() == 12345);
+	assert(cfgp.getDisplayTimeoutValue() == 30);
+	assert(cfgp.getRessources().equals("./fishes"));
+	
+	System.out.print(".");
+    }
+
+    public static void main(String args[]){
+	
+	boolean estMisAssertion = false;
+	assert estMisAssertion = true;
+
+	if (!estMisAssertion){
+		System.out.println("Execution impossible sans l'option -ea");
+		return;
+	}
+
+	TestConfigParser test = new TestConfigParser();
+	test.testParser();
+	System.out.println(" OK");
+    }
+
+}
