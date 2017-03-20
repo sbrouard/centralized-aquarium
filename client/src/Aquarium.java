95c02d609cf87957648d58d7299595c9a8c48b82
DUARTE Anthony <aduarte001@tierce.pedago.ipb.fr>
2017-03-20 10:39:23 +0100

    ahla

--- client/src/Aquarium.java
+++ client/src/Aquarium.java
@@ -31,6 +31,9 @@ public class Aquarium extends JFrame{
 	*/
 
 	Fish magicarpe = new Fish("magicarpe", "magicarpe",-50 ,200 , 200, 200);
+	Fish magicarpe2 = new Fish("magicarpe2", "magicarpe", 50, 250, 300, 300);
+
+	this.add(magicarpe2);
 	this.add(magicarpe);
 
 	
@@ -39,21 +42,55 @@ public class Aquarium extends JFrame{
 	this.setVisible(true);
 	
 	try{
-	    TimeUnit.SECONDS.sleep(2);
+	    TimeUnit.SECONDS.sleep(1);
 	} catch (Exception e){}
 	
 	magicarpe.setPosX(0);
+	this.remove(magicarpe2);
 	this.repaint();
 
     }
 
-    public void setFishes(ArrayList<Fish> fishes){
+    public void setFishes(ArrayList<Fish> f){
+	
+	for(Fish fish1 : f){
+	    
+	    boolean toAdd = true;
+	    
+	    for(Fish fish2 : this.fishes){
+		if(fish2.getName().equals(fish1.getName())){
+		    toAdd = false;
+		    break;
+		}
+	    }
+	    
+	    if(toAdd){
+		this.fishes.add(fish1);
+		this.add(fish1);
+	    }
+	}
+
+	for(Fish fish1 : this.fishes){
+	    
+	    boolean toDel = true;
+
+	    for(Fish fish2 : f){
+		if(fish1.getName().equals(fish2.getName())){
+		    toDel = false;
+		    break;
+		}
+	    }
+
+	    if(toDel)
+		this.fishes.remove(fish1);
+	}
     }
     
     public void update(){
-	for(Fish f : fishes){
+	for(Fish f : this.fishes){
 	    f.move();
 	}
+	this.repaint();
     }
 
     public static void main(String args[]){
